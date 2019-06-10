056c1b4ec8b45da5b1d759cad32e5e8c87ca98c9
Nate Tan <nktan93@gmail.com>
2019-06-10 14:05:28 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,14 @@
 require 'pry'
 
+<<<<<<< HEAD
 require_relative '../lib/concerns/findable'
 require_relative '../lib/concerns/memorable'
 require_relative '../lib/concerns/paramable'
+=======
+# require_relative '../lib/concerns/findable'
+require_relative '../lib/concerns/memorable'
+# require_relative '../lib/concerns/paramable'
+>>>>>>> c1635bd08237c2605c63c2daf7a2441af86302b7
 
 require_relative '../lib/artist.rb'
 require_relative '../lib/song.rb'
